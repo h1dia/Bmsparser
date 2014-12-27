1382ba2d1704a99abf79b973af8d9b09a945eed3
h1dia <perditus.p@gmail.com>
2014-12-27 17:00:34 +0900

    add timer

--- bmsparser/main.cpp
+++ bmsparser/main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <Windows.h>
 #include "Bmsdata.h"
 
 using namespace std;
 
 int main(){
 	Bmsdata data;
+	LARGE_INTEGER sysfreq, ltime, lresult;
 
-	data.bmspath = "dive_air04FD.bms";
-	data.setbmsstring();
+	cout << "load start" << endl;
 	
+	QueryPerformanceFrequency(&sysfreq);
+	data.bmspath = "dive_air04FD.bms";
+	QueryPerformanceCounter(&ltime);
+	data.setbmsstring();
+	QueryPerformanceCounter(&lresult);
+
 	int notes = 0;
 	for (int i = 11; i < 20; i++){
 		notes += data.getsize(i);
 	}
 
-	cout << "ƒm[ƒg”‚Í " << notes <<endl;
-
+	cout << "title   : " << data.gettitle() << endl;
+	cout << "notes   : " << notes << endl;
+	cout << "level   : " << data.getplaylevel() << endl;
+	cout << "calctime: " << (double)(lresult.QuadPart - ltime.QuadPart) / (double)sysfreq.QuadPart << "(sec)" << endl;
 	return 0;
 }
